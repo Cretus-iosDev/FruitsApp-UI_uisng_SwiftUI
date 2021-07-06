83c8e81ab59303afb6be6672f34312557344cbf2
Rutik Maraskolhe <84556881+Cretus-iosDev@users.noreply.github.com>
2021-07-06 11:25:33 +0530

    Add files via upload

---
+++ FruitsApp-UI/FruitsApp-UITests/FruitsApp_UITests.swift
@@ -0,0 +1,33 @@
+//
+//  FruitsApp_UITests.swift
+//  FruitsApp-UITests
+//
+//  Created by rutik maraskolhe on 06/07/21.
+//
+
+import XCTest
+@testable import FruitsApp_UI
+
+class FruitsApp_UITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
